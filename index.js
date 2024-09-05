1f46efbc88024fdc2f9ab5a00e767aed16ec58ef
miniafro <128392820+miniafro@users.noreply.github.com>
2024-09-05 17:23:21 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function checkPass(pass){
+    if(pass == "pes"){
+        alert("Access Granted");
+        window.location.replace('https://allium-test.skauting.cz/hmmcotoasibude');
+    }
+    else {
+  alert("Wrong password");
+    }
+}
