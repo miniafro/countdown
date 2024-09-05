9a3d8d235d00a028c8f52b439d7aa01fb193f303
miniafro <128392820+miniafro@users.noreply.github.com>
2024-09-05 18:01:27 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function checkPass(pass){
-    if(pass == "pes"){
-        alert("Access Granted");
-        window.location.replace('https://allium-test.skauting.cz/hmmcotoasibude');
-    }
-    else {
-  alert("Wrong password");
-    }
-}
